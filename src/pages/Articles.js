612514dd1c948b5b2e32e8fc8404750441ddaaf9
nzrmm <iamgata.me@gmail.com>
2022-02-22 23:12:11 +0700

    add articles.js

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,12 +1,17 @@
 import { articles } from '../utils/articles';
+import BaseCardSmall from '../base/BaseCardSmall';
 
 export default function Articles() {
   return (
     <section>
-      <div>
+      <div className="py-16 text-center">
         <h1>Ui Design</h1>
       </div>
-      <div></div>
+      <div className="grid grid-cols-3 gap-x-8 gap-y-16 py-16">
+        {articles.slice(1).map(article => {
+          return <BaseCardSmall key={article.id} children={article} />;
+        })}
+      </div>
     </section>
   );
 }
